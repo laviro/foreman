42b58ed78206e7378c4b3bc73fe379f6a1901f31
Ron Lavi <1ronlavi@gmail.com>
2021-07-04 11:39:13 +0000

    Fixes #32880 - Add confirmation modal service

--- webpack/assets/javascripts/react_app/Root/ReactApp.js
+++ webpack/assets/javascripts/react_app/Root/ReactApp.js
@@ -9,6 +9,7 @@ import AppSwitcher from '../routes';
 
 import apolloClient from './apollo';
 import ToastsList from '../components/ToastsList';
+import ConfirmModal from '../components/ConfirmModal';
 
 const ReactApp = ({ layout, metadata, toasts }) => {
   const contextData = { metadata };
@@ -22,6 +23,7 @@ const ReactApp = ({ layout, metadata, toasts }) => {
             <Layout data={layout}>
               <ToastsList railsMessages={toasts} />
               <AppSwitcher />
+              <ConfirmModal />
             </Layout>
           </ConnectedRouter>
         </ApolloProvider>
